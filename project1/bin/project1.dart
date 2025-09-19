db9e3f0ec988054b567a647b3157f43c982d92ab
Zaki Muhammad Athallah Erlangga <zakihalilintar76@gmail.com>
2025-09-19 10:59:58 +0700

    variabel

--- project1/bin/project1.dart
+++ project1/bin/project1.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:project1/project1.dart' as project1;
 const String nama = 'Zaki';
 void main(List<String> arguments) {
@@ -12,5 +14,15 @@ banyak line comment
 //constanta dan final 
 final String nama2 = 'Erlangga';
 print("Nama saya $nama $nama2"); 
-
+//variabel
+int umur = 21;
+double tinggi = 173;
+bool jenis_kelamin = true; //true = laki-laki, false = perempuan
+String alamat = 'Jl. Timor No. 56';
+stdout.writeln("Umur saya $umur tahun");
+stdout.writeln("Tinggi saya $tinggi cm");
+stdout.writeln(
+  "Jenis kelamin saya ${jenis_kelamin ? ' Laki-laki' : 'Perempuan'}",
+);
+stdout.writeln("Alamat saya $alamat");
 }
